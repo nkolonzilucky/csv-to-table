7b54291e28d9aa280ef6140c54e6b25f978107b2
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-11-14 10:38:23 +0000

    added a Row component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 
 
 interface RecordEntryType {
-date: Date;
-count: number
+  id: number;
+  date: Date;
+  count: number
 }
 
 function csvData() : RecordEntryType[] {
 const csvData: RecordEntryType[] = [
-{date: new Date(2024, 0, 31, 0, 0, 0, 0), count: 3},
-{date: new Date(2024, 1, 31, 0, 0, 0, 0), count: 12},
-{date: new Date(2024, 2, 31, 0, 0, 0, 0), count: 32},
-{date: new Date(2024, 3, 31, 0, 0, 0, 0), count: 69},
-{date: new Date(2024, 4, 31, 0, 0, 0, 0), count: 469},
-{date: new Date(2024, 5, 31, 0, 0, 0, 0), count: 569},
+{id: 0 ,date: new Date(2024, 0, 31, 0, 0, 0, 0), count: 3},
+{id: 1 ,date: new Date(2024, 1, 31, 0, 0, 0, 0), count: 12},
+{id: 2 ,date: new Date(2024, 2, 31, 0, 0, 0, 0), count: 32},
+{id: 3 ,date: new Date(2024, 3, 31, 0, 0, 0, 0), count: 69},
+{id: 4 ,date: new Date(2024, 4, 31, 0, 0, 0, 0), count: 469},
+{id: 5 ,date: new Date(2024, 5, 31, 0, 0, 0, 0), count: 569},
 ];
 return csvData
 
@@ -35,14 +36,20 @@ return (
       </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>{new Date().toDateString()}</td>
-          <td className="text-center">2</td>
-        </tr>
+        {csvData()?.map(({id, date, count}) => <Row key={id} date={date} count={count} /> )}
       </tbody>
     </table>
     </div>
   </div>
   </>
 );
+}
+
+function Row({ date, count} : { date: Date, count: number}) {
+  return (
+    <tr>
+        <td>{date.toDateString()}</td>
+        <td className="text-center">{count}</td>
+    </tr>
+  );
 }
